b8850d44613dbf55dfd2c79ffbd7c060645683e7
jinia hossen <jiniahossen31@gmail.com>
2024-01-08 21:33:37 +0600

    Randome number

--- Do.js
+++ Do.js
@@ -1,18 +1,18 @@
 // reverse a string 
 
-let sntnc='Hi kemon acho tumi';
+let sntnc = 'Hi kemon acho tumi';
 
-let convertedStnc= sntnc.split(' ').map(function(word){
+let convertedStnc = sntnc.split(' ').map(function (word) {
     return word.split('').reverse().join('')
 })
 
-let finalSntnc=convertedStnc.join(' ');
+let finalSntnc = convertedStnc.join(' ');
 // console.log(finalSntnc);
 
 //check array or object
 
-function checkArray(arr){
-     return Array.isArray(arr);
+function checkArray(arr) {
+    return Array.isArray(arr);
 }
 
 // console.log(checkArray([]));
@@ -21,40 +21,40 @@ function checkArray(arr){
 
 //How to empty and array in Javascript? Do not reset it to a new array,and do not lopp through to pop each value
 
-let arr=[1,2,3,4,5]
-arr.length=0;
+let arr = [1, 2, 3, 4, 5]
+arr.length = 0;
 // console.log(arr);
 
 //how would you check if an number is an ineteger?
 // let a=2;
 // console.log(Number.isInteger(a));
 
-function CheckInteger(num){
-    if(num%1===0){
+function CheckInteger(num) {
+    if (num % 1 === 0) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
-const check=CheckInteger(10.5);
+const check = CheckInteger(10.5);
 // console.log(check);
 
 //duplicate an array
 
-function duplicate(num){
+function duplicate(num) {
     return num.concat(num)
 }
 
 // console.log(duplicate([1,2,3,4,5,6]));
 
-function reverseNumber(num){
-    let rev=0;
-    while(num>0){
-        let rem=num%10;
-        rev=rev*10+rem;
-        num=Math.floor(num/10)
+function reverseNumber(num) {
+    let rev = 0;
+    while (num > 0) {
+        let rem = num % 10;
+        rev = rev * 10 + rem;
+        num = Math.floor(num / 10)
     }
     return rev;
 }
@@ -63,19 +63,53 @@ function reverseNumber(num){
 
 //sort a string alphabaticly
 
-const Name='Jinia';
+const Name = 'Jinia';
 console.log(Name.split('').sort().join(''));
 // const Name = [10, 2, 752, 9, 1, 3, 23, 45, 871];
 // console.log(Name.sort((a, b) => a - b));
-console.log(!""&&"pro")
+console.log(!"" && "pro")
 
-let age = 25; 
-let age2=age;
-age2=4;
+let age = 25;
+let age2 = age;
+age2 = 4;
 console.log(age);
 //Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.  
 
-function triangleArea(a,b,c){
-    return area=(a+b+c)/2;
+function triangleArea(a, b, c) {
+    return area = (a + b + c) / 2;
 }
-console.log('Triangle Area:', triangleArea(5,6,7));
+console.log('Triangle Area:', triangleArea(5, 6, 7));
+
+function isWithinRange(number) {
+    // Check if the number is within 20 of 100 or 400
+    return (Math.abs(100 - number) <= 20) || (Math.abs(400 - number) <= 20);
+}
+
+// Example usage:
+var userInput = 85; // Replace this with the integer you want to check
+if (isWithinRange(userInput)) {
+    console.log(userInput + ' is within 20 of 100 or 400.');
+} else {
+    console.log(userInput + ' is not within 20 of 100 or 400.');
+}
+
+function isPossible(x, y, z) {
+    // Check if it is possible to replace $ with +, -, *, or /
+    return (
+        x + y === z ||
+        x - y === z ||
+        x * y === z ||
+        x / y === z
+    );
+}
+
+// Example usage:
+var x = 10; // Replace this with the value of x
+var y = 30; // Replace this with the value of y
+var z = 300; // Replace this with the value of z
+
+if (isPossible(x, y, z)) {
+    console.log(`It is possible to replace $ with +, -, *, or / to obtain ${x} $ ${y} = ${z}.`);
+} else {
+    console.log(`It is not possible to replace $ with +, -, *, or / to obtain ${x} $ ${y} = ${z}.`);
+}
